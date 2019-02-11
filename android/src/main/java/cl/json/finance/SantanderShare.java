4252367cd6dc0c91401067294ca3234ff2c48e06
Priscila <you@example.com>
2019-02-11 16:37:02 -0200

    Finances

---
+++ android/src/main/java/cl/json/finance/SantanderShare.java
@@ -0,0 +1,45 @@
+package cl.json.social;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import java.io.File;
+import android.os.Environment;
+import android.net.Uri;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableMap;
+
+/**
+ * Created by Priscila Rodrigues on 11-02-18.
+ */
+public class SantanderShare extends SingleShareIntent {
+
+    private static final String PACKAGE = "com.santander.app";
+    private static final String PLAY_STORE_LINK = "market://details?id=com.santander.app";
+
+    public SantanderShare(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public void open(ReadableMap options) throws ActivityNotFoundException {
+        super.open(options);
+        //  extra params here
+        this.openIntentChooser();
+    }
+
+    @Override
+    protected String getPackage() {
+        return PACKAGE;
+    }
+
+    @Override
+    protected String getDefaultWebLink() {
+        return null;
+    }
+
+    @Override
+    protected String getPlayStoreLink() {
+        return PLAY_STORE_LINK;
+    }
+}
